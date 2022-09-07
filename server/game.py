2dfed447081c3e287f7a204b154f7abf502224ca
tymowskyy <tymowskyy@gmail.com>
2022-09-07 22:34:09 +0200

    added server-side game logic and unit tests

---
+++ server/game.py
@@ -0,0 +1,105 @@
+from enum import Enum
+
+class Orientation(Enum):
+    HORIZONTAL = False
+    VERTICAL = True
+
+    def translate(self, position, distance):
+        if self.value:
+            return (position[0], position[1] + distance - 1)
+        return (position[0] + distance - 1, position[1])
+
+class PlaceShipResponses(Enum):
+    INVALID_POSITION = 1
+    SHIP_USED = 2
+    SUCCESS = 3
+    SUCCESS_ALL_SHIPS_PLACED = 4
+
+class FireResponses(Enum):
+    INVALID_POSITION = 1
+    CAN_NOT_FIRE_YET = 2
+    MISS = 3
+    HIT = 4
+    SUNK = 5
+    WIN = 6
+
+class Game:
+
+    def __init__(self, board_size=10, ships=None):
+        if ships is None:
+            ships = [5, 4, 3, 3, 2]
+        self.board_size = board_size
+        self.ships = ships
+        self.ship_sizes_left = [self.ships.copy(), self.ships.copy()]
+        self.player_ships = [[], []]
+
+    def is_on_board(self, position):
+        return position[0] >= 0 and position[0] < self.board_size \
+            and position[1] >= 0 and position[1] < self.board_size
+
+    def place_ship(self, player, ship_size, ship_position, ship_orientation):
+        if not ship_size in self.ship_sizes_left[player]:
+            return PlaceShipResponses.SHIP_USED
+
+        if not self.is_on_board(ship_position):
+            return PlaceShipResponses.INVALID_POSITION
+        ship_end_position = ship_orientation.translate(ship_position, ship_size)
+
+        if not self.is_on_board(ship_end_position):
+            return PlaceShipResponses.INVALID_POSITION
+        
+        for x in range(ship_position[0], ship_end_position[0]+1):
+            for y in range(ship_position[1], ship_end_position[1]+1):
+                for ship in self.player_ships[player]:
+                    if ship.colide((x, y)):
+                        return PlaceShipResponses.INVALID_POSITION
+
+        self.ship_sizes_left[player].remove(ship_size)
+        self.player_ships[player].append(Ship(ship_position, ship_orientation, ship_size))
+
+
+        if self.ship_sizes_left[False] or self.ship_sizes_left[True]:
+            return PlaceShipResponses.SUCCESS
+        return PlaceShipResponses.SUCCESS_ALL_SHIPS_PLACED
+
+
+    def fire(self, player, position):
+        if not self.is_on_board(position):
+            return FireResponses.INVALID_POSITION, None
+
+        if self.ship_sizes_left[0] or self.ship_sizes_left[1]:
+            return FireResponses.CAN_NOT_FIRE_YET, None
+
+        for ship in self.player_ships[not player]:
+            if ship.colide(position):
+                sunk = ship.hit(position)
+                if sunk:
+                    if all([s.is_sunk() for s in self.player_ships[not player]]):
+                        return FireResponses.WIN, ship.size
+                    return FireResponses.SUNK, ship.size
+                return FireResponses.HIT, None
+        
+        return FireResponses.MISS, None
+
+class Ship:
+    def __init__(self, position, orientation, size):
+        self.size = size
+
+        end_position = orientation.translate(position, size)
+
+        self.tiles = []
+        for x in range(position[0], end_position[0]+1):
+            for y in range(position[1], end_position[1]+1):
+                self.tiles.append((x, y))
+        self.hitted = [False] * size
+    
+    def colide(self, position):
+        return position in self.tiles
+    
+    def hit(self, position):
+        index = self.tiles.index(position)
+        self.hitted[index] = True
+        return self.is_sunk()
+    
+    def is_sunk(self):
+        return all(self.hitted)
---
+++ server/test_game.py
@@ -0,0 +1,32 @@
+import unittest
+from game import Game, Orientation, PlaceShipResponses, FireResponses
+
+class TestGame(unittest.TestCase):
+    def test_place_ship_responses(self):
+        g = Game(ships=[2, 3])
+        self.assertEqual(g.place_ship(False, 2, (3, 3), Orientation.VERTICAL), PlaceShipResponses.SUCCESS)
+        self.assertEqual(g.place_ship(False, 2, (3, 3), Orientation.VERTICAL), PlaceShipResponses.SHIP_USED)
+        self.assertEqual(g.place_ship(False, 3, (2, 4), Orientation.HORIZONTAL), PlaceShipResponses.INVALID_POSITION)
+        self.assertEqual(g.place_ship(True, 2, (9, 9), Orientation.VERTICAL), PlaceShipResponses.INVALID_POSITION)
+        g.place_ship(True, 2, (0, 0), Orientation.VERTICAL)
+        g.place_ship(True, 3, (1, 0), Orientation.VERTICAL)
+        self.assertEqual(g.place_ship(False, 3, (5, 5), Orientation.HORIZONTAL), PlaceShipResponses.SUCCESS_ALL_SHIPS_PLACED)
+
+    def test_fire_responses(self):
+        g = Game(ships=[2, 1])
+        self.assertEqual(g.fire(False, (0, 0)), (FireResponses.CAN_NOT_FIRE_YET, None))
+        g.place_ship(False, 2, (0, 0), Orientation.HORIZONTAL)
+        g.place_ship(False, 1, (0, 2), Orientation.HORIZONTAL)
+        g.place_ship(True, 2, (3, 0), Orientation.HORIZONTAL)
+        g.place_ship(True, 1, (3, 2), Orientation.HORIZONTAL)
+        self.assertEqual(g.fire(False, (-1, 2)), (FireResponses.INVALID_POSITION, None))
+        self.assertEqual(g.fire(False, (0, 0)), (FireResponses.MISS, None))
+        self.assertEqual(g.fire(True, (0, 0)), (FireResponses.HIT, None))
+        g.fire(False, (0, 1))
+        self.assertEqual(g.fire(True, (1, 0)), (FireResponses.SUNK, 2))
+        g.fire(False, (0, 2))
+        self.assertEqual(g.fire(True, (0, 2)), (FireResponses.WIN, 1))
+
+
+if __name__ == '__main__':
+    unittest.main()
