30ead28ed49369aea31957c3a325a11e2b32281f
tymowskyy <tymowskyy@gmail.com>
2022-09-08 23:58:18 +0200

    created lobby system

--- client/battleships.py
+++ client/battleships.py
@@ -2,22 +2,66 @@ import socketio
 
 sio = socketio.Client()
 
+CREATE_OR_JOIN_LOBBY_MESSAGE = '1 - Join Lobby\n2 - Create Lobby'
+
 @sio.event
 def connect():
     print('connection established')
 
 @sio.event
-def user_joined(data):
-    print('user joined ', data)
-
-@sio.event
 def disconnect():
     print('disconnected from server')
 
+def create_or_join_lobby(user_input):
+    global after_input, current_lobby
+
+    if user_input == '1':
+        lobby_id = input('Input Lobby ID: ')
+        response = sio.call('join_lobby', {'lobby_id': lobby_id})
+        if not response['success']:
+            print('ERROR', response['error'])
+            print(CREATE_OR_JOIN_LOBBY_MESSAGE)
+            return
+
+        current_lobby = lobby_id
+        print(f'Joined Lobby {current_lobby}')        
+        after_input = None
+
+    elif user_input == '2':
+        response = sio.call('create_lobby', {})
+        if not response['success']:
+            print('ERROR', response['error'])
+            print(CREATE_OR_JOIN_LOBBY_MESSAGE)
+            return
+
+        current_lobby = response['lobby_id']
+        print(f'Your Lobby ID is {current_lobby}')
+        after_input = None
+
+    else:
+        print('Invalid input!')
+        print(CREATE_OR_JOIN_LOBBY_MESSAGE)
+
+after_input = create_or_join_lobby
+current_lobby = None
+
 def main():
     sio.connect('http://localhost:5000')
-    while True:
-        input()
+
+    print(CREATE_OR_JOIN_LOBBY_MESSAGE)
+
+    run = True
+    while run:
+        user_input = input()
+        if user_input == 'quit':
+            run = False
+            break
+        if not after_input is None:
+            after_input(user_input)
+
+
+    sio.disconnect()
+
 
 if __name__ == '__main__':
     main()
--- server/battleship/__init__.py
+++ server/battleship/__init__.py
@@ -1,5 +1,6 @@
 import socketio
 import os
+from random import randint
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
@@ -8,17 +9,45 @@ app = socketio.WSGIApp(sio, static_files={
     '/': {'content_type': 'text/html', 'filename': os.path.join(DIR_PATH, 'index.html')}
 })
 
-users = []
+lobbies = {}
+
+def generate_unique_lobby_id(lenght = 6):
+    generate_lobby_id = lambda : ''.join([str(randint(0, 10)) for _ in range(lenght)])
+    lobby_id = generate_lobby_id()
+    while lobby_id in lobbies:
+        lobby_id = generate_lobby_id()
+    return lobby_id
 
 @sio.event
 def connect(sid, environ):
     print(f'{sid} connected')
-    for user in users:
-        print('sent')
-        sio.emit('user_joined', {'n_users': len(users) + 1}, to=user)
-    users.append(sid)
 
 @sio.event
 def disconnect(sid):
-    users.remove(sid)
     print(f'{sid} disconnect')
+
+@sio.event
+def join_lobby(sid, data):
+    lobby_id = data['lobby_id']
+    if not lobby_id in lobbies:
+        return {'success': False, 'error': 'Lobby with this ID does not exist!'}
+    if len(lobbies[lobby_id]) == 2:
+        return {'success': False, 'error': 'Lobby is full!'}
+
+    with sio.session(sid) as session:
+        if 'lobby_id' in session:
+            return {'success': False, 'error': 'User is already in another lobby!'}
+        session['lobby_id'] = lobby_id
+    lobbies[lobby_id].append(sid)
+    return {'success': True}
+
+
+@sio.event
+def create_lobby(sid, data):
+    new_lobby_id = generate_unique_lobby_id()
+    with sio.session(sid) as session:
+        if 'lobby_id' in session:
+            return {'success': False, 'error': 'User is already in another lobby!'}
+        session['lobby_id'] = new_lobby_id
+    lobbies[new_lobby_id] = [sid]
+    return {'success': True, 'lobby_id': new_lobby_id}
